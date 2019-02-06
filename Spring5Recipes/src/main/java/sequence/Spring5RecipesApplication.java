3602138de893ba654efd3e93d5189e8136c7f4ce
natrajanrajasekaran <natrajan.rajasekaran@icloud.com>
2019-02-06 22:19:53 +0800

    Configuration based sequence generator # 1

--- Spring5Recipes/src/main/java/sequence/Spring5RecipesApplication.java
+++ Spring5Recipes/src/main/java/sequence/Spring5RecipesApplication.java
@@ -2,12 +2,22 @@ package sequence;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import sequence.config.SequenceGeneratorConfiguration;
 
 @SpringBootApplication
 public class Spring5RecipesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Spring5RecipesApplication.class, args);
+		
+		ApplicationContext context = new AnnotationConfigApplicationContext(SequenceGeneratorConfiguration.class);
+		SequenceGenerator generator = context.getBean(SequenceGenerator.class);
+		System.out.println(generator.getSequence());
+		System.out.println(generator.getSequence());
+		
+		//SpringApplication.run(Spring5RecipesApplication.class, args);
 	}
 
 }
---
+++ Spring5Recipes/src/main/java/sequence/config/SequenceGeneratorConfiguration.java
@@ -0,0 +1,19 @@
+package sequence.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import sequence.SequenceGenerator;
+
+@Configuration
+public class SequenceGeneratorConfiguration {
+	
+	@Bean
+	public SequenceGenerator sequenceGenerator() {
+		SequenceGenerator seqgen = new SequenceGenerator();
+		seqgen.setPrefix("30");
+		seqgen.setSuffix("A");
+		seqgen.setInitial(100000);
+		return seqgen;
+	}
+}
